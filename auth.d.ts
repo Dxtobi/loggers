a9384dd83e4e820392b02e672502b6bbaf9e29e1
dxtobi <akanbijosephtobi@gmail>
2023-02-14 07:41:13 -0800

    first commit

--- auth.d.ts
+++ auth.d.ts
@@ -2,14 +2,4 @@ import "next-auth";
 
 
 
-declare global {
-    interface User {
-        _id:string
-    }
 
-    module "next-auth" {
-        interface Session {
-            user:User
-        }
-    }
-}
