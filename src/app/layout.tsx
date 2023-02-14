393920f6de85f3549462701644bfef081561035b
dxtobi <akanbijosephtobi@gmail>
2023-02-14 08:41:36 -0800

    not the first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,13 @@ import { SessionProvider } from "next-auth/react"
 import AppBar from './components/header/AppBar';
 import { Session } from 'next-auth';
 
+
 export default function RootLayout({
   children,
   session
 }: {
     children: React.ReactNode,
-    session: Session
+    session: any
 }) {
   return (
     <html lang="en">
